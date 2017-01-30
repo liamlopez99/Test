f7ab52e85041c216c4446d5a124e557e306b903e
Morgan Reid <morganreidhhs@gmail.com>
2017-01-30 09:19:40 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hhs_rm51/gitttt/GIT_REKT.java
@@ -0,0 +1,14 @@
+package com.example.hhs_rm51.gitttt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GIT_REKT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git__rekt);
+        //Liam
+    }
+}
